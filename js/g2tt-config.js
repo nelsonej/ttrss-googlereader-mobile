ae9bb12c9db207d4a7ce355bb5556179d0298254
Eric Nelson <eric.j.nelson@gmail.com>
2022-07-19 08:17:36 -0700

    Updates for my site

--- js/g2tt-config.js
+++ js/g2tt-config.js
@@ -1,12 +1,12 @@
 // Config
-global_ttrssUrl = "/ttrss";
+global_ttrssUrl = "/tt-rss";
 
 // Preferences
 pref_Feed = '-4'; // Default: all items
 pref_ViewMode = 'unread'; // Default: unread articles only
 // Not used -- pref_TextType = 'content'; // Default: full articles
-pref_OrderBy = 'feed_dates'; // feed_dates: Sort by newest; date_reverse: Sort by oldest
+pref_OrderBy = 'date_reverse'; // feed_dates: Sort by newest; date_reverse: Sort by oldest
 pref_FeedSort = '0'; // Default: Do not sort feeds
-pref_StartInCat = '0'; // Default: Start showing feeds
+pref_StartInCat = '1'; // Default: Start showing feeds
 //added so user can change feed limit easily.
 pref_Feed_limit = 25; // Default: Load 25 Items...
